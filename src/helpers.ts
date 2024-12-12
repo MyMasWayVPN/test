a9e65b79e87cb5a9d067d06b1eecbe20202d238e
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2024-12-13 06:14:22 +0700

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,22 +1,25 @@
-import { getUrl, Result, allowedMethod } from './utils'
-import callAPI from './routing'
+import { getUrl, allowedMethod } from './utils';
+import callAPI from './routing';
 
-export default async function serveResult(request: Request): Promise<Response> {
-  const dc = getUrl(request).searchParams.get('decode')
-  let code = 200
-  let result: Result = await callAPI(request);
+export default async function serveResult(request) {
+  const dc = getUrl(request).searchParams.get('decode');
+  let code = 200;
+  let result = await callAPI(request);
+
   if (result.name) {
-    result.name = result.name.replace(/\u002B/g, '%20')
+    result.name = result.name.replace(/\u002B/g, '%20');
     if (dc === null || dc === 'true' || dc !== 'false') {
-      result.name = decodeURIComponent(result.name)
+      result.name = decodeURIComponent(result.name);
     }
   }
+
   if (result.message === 'Bad request') {
-    code = 400
+    code = 400;
   }
   if (result.message === 'Not found') {
-    code = 404
+    code = 404;
   }
+
   const response = new Response(JSON.stringify(result), {
     status: code,
     headers: {
@@ -27,6 +30,7 @@ export default async function serveResult(request: Request): Promise<Response> {
       'Content-Type': 'application/json; charset=utf-8',
       'X-Powered-By': '@ihsangan/valid'
     }
-  })
-  return response
+  });
+
+  return response;
 }
